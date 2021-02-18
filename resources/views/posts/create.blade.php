16461c3ec6333f858ecdd6ce968b7caaf2ce495f
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-02-18 15:51:01 -0600

    4_6 formulario

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Crear Articulo</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                <form
+                action="{{ route('posts.store')}}"
+                 method="POST"
+                 enctype="multiparr/form-date"
+                 >
+                    <div class="form-group">
+                        <label >Titulo *</label>
+                        <input type="text" name="title" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label >Image *</label>
+                        <input type="file" name="file" >
+                    </div>
+                    <div class="form-group">
+                        <label >Cotenido *</label>
+                        <textarea name="body" rows="6" class="form-control" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label >Cotenido embebido</label>
+                        <textarea name="iframe"  class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                            @csrf
+                            <input type="submit" value="Enviar" class="btn btn-primary">
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
