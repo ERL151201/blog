6e5c4d94de1608a984275e1c3bb8106aca060867
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-02-19 20:11:06 -0600

    4_8 editar

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -54,16 +54,6 @@ class PostController extends Controller
         return back()->with('status', 'Creado con éxito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -73,7 +63,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('posts.edit', compact('post'));
     }
 
     /**
